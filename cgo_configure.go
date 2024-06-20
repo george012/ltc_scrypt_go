1954bba957b8430686f1771fed98a675bd950d42
WMYeah_Mac <george017@163.com>
2024-06-20 21:32:52 +0800

    dw

--- cgo_configure.go
+++ cgo_configure.go
@@ -21,7 +21,7 @@ package ltc_scrypt_go
 
 #cgo windows,amd64 CFLAGS: -I${SRCDIR}/lib/windows -I${SRCDIR}/lib/include
 #cgo windows,amd64 CPPFLAGS: -I${SRCDIR}/lib/windows -I${SRCDIR}/lib/include
-#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/windows/libscrypt.a C:/msys64/mingw64/lib/libssl.a C:/msys64/mingw64/lib/libcrypto.a
+#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/windows/libscrypt.a /mingw64/lib/libssl.a /mingw64/lib/libcrypto.a
 
 #cgo windows,arm64 CFLAGS: -I${SRCDIR}/lib/windows -I${SRCDIR}/lib/include
 #cgo windows,arm64 CPPFLAGS: -I${SRCDIR}/lib/windows -I${SRCDIR}/lib/include
