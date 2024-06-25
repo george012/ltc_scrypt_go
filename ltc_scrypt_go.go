e1dd7c5cab781cf699bac1ea668d9f5bbfabf8e1
WMYeah_NUC <george017@163.com>
2024-06-25 09:47:08 +0800

    Update v0.0.6

--- ltc_scrypt_go.go
+++ ltc_scrypt_go.go
@@ -1,3 +1,18 @@
+// Package ltc_scrypt_go
+/*
+	en: provides functions to compute scrypt hash and related utilities.
+	zh-CN: 提供了计算 scrypt 哈希及相关工具的功能。
+*/
+package ltc_scrypt_go
+
+import "C"
+import (
+	"encoding/hex"
+	"sync"
+	"unsafe"
+)
+en: Input byte array; zh-CN: 输入的字节数组;
+*/
 package ltc_scrypt_go
 
 /*
@@ -7,10 +22,23 @@ package ltc_scrypt_go
 import "C"
 import (
 	"encoding/hex"
+	"sync"
 	"unsafe"
 )
 
+var mutex sync.Mutex
+
+// CalcScryptHash
+/*
+	en: computes the scrypt hash of the input bytes;
+	zh-CN: 计算输入字节的scrypt哈希;
+ 	@params bytesInput en: Input byte array; zh-CN: 输入的字节数组;
+	@return en: Scrypt hash byte array; zh-CN: scrypt哈希字节数组;
+*/
 func CalcScryptHash(bytesInput []byte) []byte {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	// avoid of the situation of empty bytes array
 	bytesInput = append(bytesInput, byte(0x0))
 	input := (*C.char)(unsafe.Pointer(&bytesInput[0]))
@@ -23,7 +51,17 @@ func CalcScryptHash(bytesInput []byte) []byte {
 	return bytesOutput
 }
 
+// CalcScryptHashHex
+/*
+	en: computes the scrypt hash of the input bytes and returns it as a hexadecimal string;
+	zh-CN: 计算输入字节的scrypt哈希并以十六进制字符串返回;
+	@params bytesInput en: Input byte array; zh-CN: 输入的字节数组;
+	@return en: Scrypt hash hexadecimal string; zh-CN: scrypt哈希十六进制字符串;
+*/
 func CalcScryptHashHex(bytesInput []byte) string {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	bytesRet := CalcScryptHash(bytesInput)
 	return hex.EncodeToString(bytesRet)
 }
